db7ff620ecad65ae14b004c0440bb69a95098b98
ian <paxian7@gmail.com>
2016-06-29 01:02:25 +0900

    commit 1

---
+++ hello/src/hello/HelloW.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world 1");
+		System.out.println("Hello world 2");
+	}
+}
